a5619bb77b504543d3b6575561dba3e5f775bf48
eiffelaqila <13520074@std.stei.itb.ac.id>
2024-02-18 14:25:19 +0700

    feat: integrate frontend & backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import axios from "axios";
+import { FaLock, FaUnlock } from "react-icons/fa"
 
 function App() {
   const backendUrl = process.env.REACT_APP_BACKEND_URL;
@@ -6,55 +8,71 @@ function App() {
     inputType: "text",
     function: "vigenere",
   })
-
+  const [cipherType, setCipherType] = useState('');
   const [output, setOutput] = useState(undefined);
 
   const mapDataToAxiosValue = (d) => {
-    let data = {
-      inputType: d.inputType,
-      function: d.function,
-      key: d.key,
-      cipherType: d.cipherType,
+    const data = {};
+    if (d.inputType === "text") {
+      data.inputText = d.inputText;
+    } else {
+      data.file = d.file;
     }
-  
-    if (data.inputType === "text") {
-      return ({
-        ...data,
-        inputText: d.inputText,
-      })
+
+    if (d.function === "affine") {
+      data.keyA = d.keyA;
+      data.keyB = d.keyB;
+    } else {
+      data.key = d.key;
     }
     
-    return ({
-      ...data,
-      file: d.file,
-    })
+    return data;
   }
 
   const handleChange = (e) => {
     let newData = {...data};
     newData[e.target.id] = e.target.value;
-
-    console.log(`Setting new value: ${e.target.value} for ${e.target.id}`)
     setData(newData);
   }
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    
+    const mappedData = mapDataToAxiosValue(data);
     const cipherType = e.nativeEvent.submitter.value;
-
-    console.log(`Pressed button: ${cipherType}`)
-    setOutput(mapDataToAxiosValue({
-      ...data,
-      cipherType,
-    }));
+    setCipherType(cipherType);
+    
+    let response = {};
+    if (data.inputType === "text") {
+      response = await axios.post(
+        `${backendUrl}/${data.function}/${cipherType}`,
+        mappedData
+      )
+    } else {
+      // TODO: File handling
+      response = await axios.post(
+        `${backendUrl}/${data.function}/${cipherType}-file}`,
+        mappedData
+      )
+    }
+  
+    setOutput(response.data);
   }
 
-  const showIfType = (type) => data.inputType === type ? "" : "hidden";
+  const isType = (type) => data.inputType === type;
+  const isFunction = (fun) => data.function === fun;
 
   return (
     <div className="relative flex flex-col justify-center min-h-screen bg-white">
-      <div className="w-full p-6 m-auto lg:max-w-xl">
-        <h1 className="text-3xl font-bold">Classic Cipher</h1>
+      <div className="w-full px-6 py-10 m-auto lg:max-w-xl">
+        <h1 className="text-3xl font-bold text-gray-700">Tugas 1 IF4020 - Kriptografi</h1>
+        <h3 className="pt-2 font-medium leading-normal text-gray-500 text-md">
+          Oleh:
+          <br />
+          - 13520074 - Eiffel Aqila Amarendra
+          <br />
+          - 13520125 - Ikmal Alfaozi
+        </h3>
         <div className="w-full py-2 m-auto">
           <form onSubmit={(e) => handleSubmit(e)} className="flex flex-col gap-2 mt-6">
             <div>
@@ -64,11 +82,11 @@ function App() {
                 <option value="file">File</option>
               </select>
             </div>
-            <div className={showIfType("text")}>
+            <div className={isType("text") ? "" : "hidden"}>
               <label htmlFor="inputText">Plaintext / Ciphertext</label>
               <input type="text" id="inputText" placeholder="Enter text" onChange={(e) => handleChange(e)} required={data.inputType === "text"} />
             </div>
-            <div className={showIfType("file")}>
+            <div className={isType("file") ? "" : "hidden"}>
               <label htmlFor="file">File</label>
               <input type="file" id="file" placeholder="Enter file" onChange={(e) => handleChange(e)} required={data.inputType === "file"} />
             </div>
@@ -84,25 +102,34 @@ function App() {
                 <option value="super">Super Enkripsi</option>
               </select>
             </div>
-            <div>
+            <div className={!isFunction('affine')  ? "" : "hidden"}>
               <label htmlFor="key">Key</label>
-              <input type="text" id="key" placeholder="Enter key" onChange={(e) => handleChange(e)} required />
+              <input type="text" id="key" placeholder="Enter key" onChange={(e) => handleChange(e)} required={data.function !== 'affine'} />
             </div>
-            <div className="flex gap-2 mt-6">
+            <div className={isFunction('affine')  ? "" : "hidden"}>
+              <label htmlFor="keyA">Key A (Affine only)</label>
+              <input type="text" id="keyA" placeholder="Enter key a" onChange={(e) => handleChange(e)} required={data.function === 'affine'} />
+            </div>
+            <div className={isFunction('affine')  ? "" : "hidden"}>
+              <label htmlFor="keyB">Key B (Affine only)</label>
+              <input type="text" id="keyB" placeholder="Enter key b" onChange={(e) => handleChange(e)} required={data.function === 'affine'} />
+            </div>
+            <div className="flex gap-2 my-6">
               <button type="submit" value="encrypt" className="bg-indigo-700 hover:bg-indigo-600 focus:bg-indigo-600">
-                Encrypt
+                <FaLock /> Encrypt
               </button>
               <button type="submit" value="decrypt" className="bg-orange-700 hover:bg-orange-600 focus:bg-orange-600">
-                Decrypt
+                <FaUnlock /> Decrypt
               </button>
             </div>
           </form>
         </div>
         {output && (
           <div className="w-full py-2 m-auto">
-            <h2 className="text-xl font-semibold">Output</h2>
-            <p><strong>Accessing backend url:</strong> {backendUrl}</p>
-            <p><strong>Input object:</strong> {JSON.stringify(output, null, 2)}</p>
+            <h2 className="text-xl font-bold text-gray-700">{cipherType === 'encrypt' ? 'Encrypted' : 'Decrypted'} Text</h2>
+            <div className="w-full p-4 mt-4 overflow-x-scroll border border-gray-300 rounded-md bg-gray-50">
+              <p>{output.result}</p>
+            </div>
           </div>
         )}
       </div>      
